56d37b391ea9f1bc4d299506bbabeba9fd8a4686
Matthias Dive <matthias.dive@viacesi.fr>
2023-01-12 16:40:18 +0100

    add catalogue, formation & section models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Catalogue, Formation, Section
 
 # Register your models here.
+admin.site.register(Catalogue)
+admin.site.register(Formation)
+admin.site.register(Section)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class User(BaseUserManager):
+
+    def create_user(self, email, first_name, last_name, password, **other_fields):
+        if not email:
+            raise ValueError("You must provide your email adress")
+
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, first_name=first_name, last_name=last_name, password=password, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('SuperUser must be assigned to is_staff=True')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('SuperUser must be assigned to is_superuser=True')
+
+        return self.create_user(email, first_name, last_name, password, **other_fields)
+
+
+class Catalogue(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Formation(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    thumbnail = models.ImageField()
+    price = models.FloatField()
+    status = models.BooleanField()
+    tag = models.CharField(max_length=50)
+    id_catalogue = models.ForeignKey(Catalogue, on_delete=models.CASCADE, null=False, unique=False)
+
+    def __str__(self):
+        return self.name
+
+
+class CodeFormation(models.Model):
+    code = models.IntegerField(null=False, unique=True)
+    id_formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.CharField(max_length=250)
+    is_active = models.BooleanField()
+    id_formation = models.ForeignKey(Formation, on_delete=models.CASCADE)
